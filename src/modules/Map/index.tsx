c03aa9ffa7fa53d429cb53aecd025e7a479ee7b0
Cipriano99 <samuelcipriano99@gmail.com>
2024-01-27 14:10:54 -0300

    add map on continer on map page

--- src/modules/Map/index.tsx
+++ src/modules/Map/index.tsx
@@ -1,10 +1,50 @@
+import { Loader } from '@googlemaps/js-api-loader'
+
 import { Header } from './components/Header/index.'
 import * as S from './styles'
+import { useEffect, useRef } from 'react'
 
 export const MapsSection = () => {
+  const mapRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const initMap = async () => {
+      const loader = new Loader({
+        apiKey: process.env.NEXT_PUBLIC_MAPS_API_KEY as string,
+        version: 'weekly',
+      })
+
+      const { Map } = await loader.importLibrary('maps')
+      const { Marker } = (await loader.importLibrary(
+        'marker',
+      )) as google.maps.MarkerLibrary
+
+      const position = {
+        lat: -6.7779134,
+        lng: -43.0307148,
+      }
+
+      const mapOptions: google.maps.MapOptions = {
+        center: position,
+        zoom: 15,
+        mapId: 'MY_NEXT_JS_MAP_ID',
+      }
+
+      const map = new Map(mapRef.current as HTMLDivElement, mapOptions)
+
+      const marker = new Marker({
+        map,
+        position,
+      })
+    }
+
+    initMap()
+  }, [])
+
   return (
     <S.Container>
       <Header />
+      <div className="map" ref={mapRef} />
     </S.Container>
   )
 }
--- src/modules/Map/styles.ts
+++ src/modules/Map/styles.ts
@@ -3,4 +3,13 @@ import { styled } from '@/commons/styles'
 export const Container = styled('div', {
   height: '100vh',
   width: '100%',
+
+  '.map': {
+    flex: '1',
+    height: 'min(75vh, 720px)',
+    width: '100%',
+    marginTop: 24,
+    marginBottom: 24,
+    borderRadius: '$lg',
+  },
 })
